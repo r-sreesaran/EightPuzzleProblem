e4efc1f97fe1d18cae55cda75e68f7631de56f5e
r.sreesaran <r.sreesaran@gmail.com>
2014-11-05 18:47:12 +0530

    Added the logic for isSolvable

--- src/eightpuzzleproblem/Solver.java
+++ src/eightpuzzleproblem/Solver.java
@@ -5,18 +5,41 @@
  */
 package eightpuzzleproblem;
 
+import static eightpuzzleproblem.Board.blocks;
+
+
 /**
  *
  * @author sree
  */
 public class Solver {
-
+   Board intial;
     public Solver(Board initial) // find a solution to the initial board (using the A* algorithm) 
     {
+       this.intial =  intial; 
+       
+       
     }
 
     public boolean isSolvable() // is the initial board solvable? 
-    {
+    {  
+        int single[] = new int[intial.dimension()*intial.dimension()];
+        for(int row =0,tracker=0;row<intial.dimension();row++) {
+         for(int column =0; column<intial.dimension();column++,tracker++) {
+             single[tracker] = blocks[row][column];
+         }
+        }
+        int temp=0;
+        for(int count=0;count<intial.dimension()*intial.dimension();count++) {
+          for(int counter=count+1;counter<intial.dimension()*intial.dimension();counter++) 
+          {
+            if(single[count]<single[counter]) {
+              temp++;
+            }
+          }
+        }
+        if(temp%2==0) 
+            return true;
         return false;
     }
 
